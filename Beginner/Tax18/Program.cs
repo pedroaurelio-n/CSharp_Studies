507c7d197585d9c4eede4054cabca7ca00edf328
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2021-08-31 19:20:32 -0300

    Beginner exercise: Product total price calculator (18% tax)

---
+++ Beginner/Tax18/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Tax18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("All products have an 18% tax added to the original price");
+            Console.Write("Enter the product's original price: $");
+            float originalPrice = float.Parse(Console.ReadLine());
+
+            float taxValue = 0.18f;
+            float totalPrice = originalPrice * (1 + taxValue);
+
+            Console.WriteLine($"\nTax percentage: {taxValue * 100}%");
+            Console.WriteLine($"Total price: ${totalPrice.ToString("0.00")}");
+
+            Console.ReadLine();
+        }
+    }
+}
