c8ecd5c509518a4a9f82d510144cf73db54a741c
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2021-08-31 19:20:04 -0300

    Beginner exercise: Salary calculator ($20 per hour)

---
+++ Beginner/Salary20/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Salary20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 hour of work = $20");
+            Console.Write("Enter how many hours you worked: ");
+            int workHours = int.Parse(Console.ReadLine());
+
+            int salaryValue = workHours * 20;
+            Console.WriteLine($"\nBy working {workHours} hours you earned ${salaryValue}");
+
+            Console.ReadLine();
+        }
+    }
+}
