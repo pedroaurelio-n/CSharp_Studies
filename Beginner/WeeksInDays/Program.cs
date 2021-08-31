f55f518d70a3098f612b9a164f9886f8cdcabb3a
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2021-08-31 18:53:18 -0300

    Beginner exercise (Number of days in X weeks)

---
+++ Beginner/WeeksInDays/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WeeksInDays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("There are 7 days in 1 week");
+            Console.Write("Enter a number of weeks: ");
+            int weeksNumber = int.Parse(Console.ReadLine());
+
+            int daysNumber = weeksNumber * 7;
+            Console.WriteLine($"In {weeksNumber} weeks there are {daysNumber} days.");
+
+            Console.ReadLine();
+        }
+    }
+}
