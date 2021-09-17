4c960e746bd5aa68dc38c17ab6294dfcd689931c
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2021-09-16 22:37:44 -0300

    Beginner exercise: Even or Uneven

---
+++ Beginner Exercises/9_EvenUneven/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _9_EvenUneven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+                Console.WriteLine($"{number} is even!");
+
+            else
+                Console.WriteLine($"{number} is uneven!");
+
+            Console.ReadKey();
+        }
+    }
+}
