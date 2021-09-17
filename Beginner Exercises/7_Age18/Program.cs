4cee3c6e304ed466d43ceff90255524a74325f7e
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2021-09-16 22:25:16 -0300

    Beginner exercise: 18 Years Old Checker

---
+++ Beginner Exercises/7_Age18/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _7_Age18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age >= 18)
+                Console.WriteLine("You're eighteen or older!");
+
+            else
+                Console.WriteLine("You're under eighteen!");
+
+            Console.ReadKey();
+        }
+    }
+}
