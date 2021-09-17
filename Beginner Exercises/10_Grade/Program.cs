545e4fbb6bddba7c06a5898fcb914c783d6b42ac
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2021-09-17 15:39:02 -0300

    Beginner exercise: Grade

---
+++ Beginner Exercises/10_Grade/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _10_Grade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your grade (A/B/C): ");
+            char grade = char.Parse(Console.ReadLine());
+
+            if (grade == 'A')
+                Console.WriteLine("Congratulations! You reached max score.");
+
+            else if (grade == 'B')
+                Console.WriteLine("You scored average. Good luck next time.");
+
+            else if (grade == 'C')
+                Console.WriteLine("You failed. Try again.");
+
+            else
+                Console.WriteLine("Invalid grade!");
+
+            Console.ReadKey();
+        }
+    }
+}
