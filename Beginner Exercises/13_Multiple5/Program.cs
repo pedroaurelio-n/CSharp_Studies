14c9be5c316e7c3fd579f6885420b7d906141202
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2021-09-17 18:20:56 -0300

    Beginner exercise: Multiple of 5

---
+++ Beginner Exercises/13_Multiple5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _13_Multiple5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 5 == 0)
+                Console.WriteLine($"{number} is a multiple of 5");
+
+            else
+                Console.WriteLine($"{number} is not a multiple of 5");
+
+            Console.ReadKey();
+        }
+    }
+}
