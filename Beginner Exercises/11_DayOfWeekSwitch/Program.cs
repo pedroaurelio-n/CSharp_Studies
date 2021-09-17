81d3e28f65ae4e394179aa09c999fd4c129915df
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2021-09-17 17:49:34 -0300

    Beginner exercise: Day of the Week

---
+++ Beginner Exercises/11_DayOfWeekSwitch/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _11_DayOfWeekSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter day number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            switch (number)
+            {
+                case 1: Console.WriteLine("Sunday"); break;
+                case 2: Console.WriteLine("Monday"); break;
+                case 3: Console.WriteLine("Tuesday"); break;
+                case 4: Console.WriteLine("Wednesday"); break;
+                case 5: Console.WriteLine("Thursday"); break;
+                case 6: Console.WriteLine("Friday"); break;
+                case 7: Console.WriteLine("Saturday"); break;
+                default: Console.WriteLine("Invalid number"); break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
