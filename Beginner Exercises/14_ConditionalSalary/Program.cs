5299fbc53dd21afa756e12a96f43c2599aee4aa0
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2021-09-20 14:40:29 -0300

    Beginner exercise: Salary calculator with condition

---
+++ Beginner Exercises/14_ConditionalSalary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _14_ConditionalSalary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Salary Calculator\n30 work hours or more = $20/h\nLess than 30 work hours = $10/h\n");
+
+            Console.Write("Enter your work hours: ");
+            int workHours = int.Parse(Console.ReadLine());
+            float salary;
+
+            if (workHours >= 30)
+                salary = workHours * 20;
+
+            else
+                salary = workHours * 10;
+
+            Console.WriteLine($"\nYour salary is ${salary}");
+
+            Console.ReadKey();
+        }
+    }
+}
