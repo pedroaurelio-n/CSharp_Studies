4b974508c9fd50bf8bcbab9b809c964c3156281e
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2021-09-21 20:46:42 -0300

    Beginner exercise: Triple

---
+++ Beginner Exercises/17_Triple/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _17_Triple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number to triple it or enter 0 to exit: ");
+            int number = int.Parse(Console.ReadLine());
+
+            while (number != 0)
+            {
+                Console.WriteLine($"The triple of {number} is {number * 3}\n");
+
+                Console.Write("Enter a number to triple it or enter 0 to exit: ");
+                number = int.Parse(Console.ReadLine());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
