c01b68d6f276579af54af46c943239771d719cf9
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2021-09-21 20:51:21 -0300

    Beginner exercise: Even or uneven with while

---
+++ Beginner Exercises/18_EvenUnevenWhile/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _18_EvenUnevenWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a positive number to check even/uneven\nor enter a negative to exit: ");
+            int number = int.Parse(Console.ReadLine());
+
+            while (number >= 0)
+            {
+                if (number % 2 == 0)
+                    Console.WriteLine($"{number} is even!\n");
+
+                else
+                    Console.WriteLine($"{number} is uneven!\n");
+
+                Console.Write("Enter a positive number to check even/uneven\nor enter a negative to exit: ");
+                number = int.Parse(Console.ReadLine());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
