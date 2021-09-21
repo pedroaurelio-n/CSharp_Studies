140d9a0077f42ec5d347cecdad7e85127a373957
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2021-09-21 20:39:58 -0300

    Beginner exercise: Sum with while

---
+++ Beginner Exercises/16_SumWhile/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _16_SumWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter how many numbers you want to add: ");
+            int counterInt = int.Parse(Console.ReadLine());
+            int counter = counterInt;
+            int sum = 0;
+
+            Console.WriteLine("Enter the numbers:");
+
+            while (counter > 0)
+            {
+                sum += int.Parse(Console.ReadLine());
+                counter--;
+            }
+
+            Console.WriteLine($"\nThe sum of all the {counterInt} numbers is {sum}");
+
+            Console.ReadKey();
+        }
+    }
+}
