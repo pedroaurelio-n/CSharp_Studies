579fb9e5f5d28492a4dd2cb2d4b5d5df1aba3471
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2021-09-21 21:00:40 -0300

    Beginner exercise: positive and negative counter with do while

---
+++ Beginner Exercises/19_DoWhileCounter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _19_DoWhileCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Enter numbers to count how many are positive and how many are negative\nor type 0 to stop counting:");
+
+            int posCounter = 0;
+            int negCounter = 0;
+
+            do
+            {
+                number = int.Parse(Console.ReadLine());
+
+                if (number > 0)
+                    posCounter++;
+
+                else if (number < 0)
+                    negCounter++;
+
+            } while (number != 0);
+
+            Console.WriteLine($"\nPositive numbers: {posCounter}\nNegative numbers: {negCounter}");
+
+            Console.ReadKey();
+        }
+    }
+}
