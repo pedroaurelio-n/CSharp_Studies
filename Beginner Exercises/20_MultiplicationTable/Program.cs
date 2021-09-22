344f7c3e92fde2bfbaeb236c8a5267c14f649ebe
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2021-09-21 21:09:14 -0300

    Beginner exercise: Multiplication table with for

---
+++ Beginner Exercises/20_MultiplicationTable/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _20_MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Multiplication Table Checker\n");
+
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter a limit: ");
+            int limit = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nTABLE OF {number} FROM 0 TO {limit}");
+
+            for (int counter = 0; counter <= limit; counter++)
+            {
+                Console.WriteLine($"{number} X {counter} = {number * counter}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
