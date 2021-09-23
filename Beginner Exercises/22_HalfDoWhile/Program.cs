90e56acad343b74ed6482d2c1afb7c6485fbd975
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2021-09-22 21:45:04 -0300

    Beginner exercise: Check half with do while

---
+++ Beginner Exercises/22_HalfDoWhile/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _22_HalfDoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float number;
+            float half;
+
+            do
+            {
+                Console.Write("Enter a positive number to check its half\nor type a negative number to exit: ");
+                number = int.Parse(Console.ReadLine());
+
+                half = number / 2;
+
+                if (number < 0)
+                    break;
+
+                Console.WriteLine($"The half of {number} is {half}\n");
+
+            } while (number >= 0);
+
+            Console.ReadKey();
+        }
+    }
+}
