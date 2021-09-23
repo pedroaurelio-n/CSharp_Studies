b055611fd68c0d5b95f647775c7a24ea375cd40f
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2021-09-22 21:48:37 -0300

    Beginner exercise: Sum of 5 numbers with for

---
+++ Beginner Exercises/23_5NumberSum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _23_5NumberSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int sum = 0;
+
+            Console.WriteLine("Enter 5 numbers:");
+
+            for (int i = 0; i < 5; i++)
+            {
+                number = int.Parse(Console.ReadLine());
+                sum += number;
+            }
+
+            Console.WriteLine($"The total sum is {sum}");
+
+            Console.ReadKey();
+        }
+    }
+}
