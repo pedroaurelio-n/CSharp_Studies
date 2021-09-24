c0753aac24f5d0cd688978d8583f1308d0ca647a
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2021-09-23 22:43:06 -0300

    Beginner exerise: Array of numbers

---
+++ Beginner Exercises/24_ArrayNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _24_ArrayNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 31, 37, 34, 46, 20, 42, 16, 10, 30, 26, 38};
+
+            foreach(int number in numbers)
+            {
+                if (number > 18 && number < 35)
+                    Console.WriteLine(number);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
