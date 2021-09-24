6f1740e9cadac6fa1bfe7a4384c9ec5982fc3837
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2021-09-23 22:58:39 -0300

    Beginner exercise: Positive numbers in a random array

---
+++ Beginner Exercises/25_ArrayPositives/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _25_ArrayPositives
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Generating numbers within -100 and 100...");
+
+            int[] numbers = new int[10];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Random random = new Random();
+                numbers[i] = random.Next(-100, 101);
+            }
+
+            int positives = 0;
+            string arrayString = "";
+
+            foreach (int number in numbers)
+            {
+                arrayString += number.ToString();
+                arrayString += ", ";
+
+                if (number > 0)
+                    positives++;
+            }
+
+            arrayString.Substring(arrayString.Length - 2);
+
+
+            Console.WriteLine($"\nIn the array: {arrayString}");
+            Console.WriteLine($"There are {positives} positive numbers!");
+
+            Console.ReadKey();
+        }
+    }
+}
