52edcf33904e1cb6a211aebe85a188e10e262774
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2021-09-24 14:30:47 -0300

    Beginner exercise: Sum of numbers greater than 15 in an array

---
+++ Beginner Exercises/28_SumArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _28_SumArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 5, 8, 6, 4, 8, 25, 4, 2, 8, 12, 45, 12, 6, 7, 8 };
+
+            int sum = 0;
+
+            foreach (int number in numbers)
+            {
+                if (number > 15)
+                    sum += number;
+            }
+
+            Console.WriteLine($"In the array, the sum of all numbers greater than 15 is {sum}");
+
+            Console.ReadKey();
+        }
+    }
+}
