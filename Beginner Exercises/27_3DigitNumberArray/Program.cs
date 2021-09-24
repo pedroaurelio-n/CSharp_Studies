cbce49b96547d7ecf3744d4defd0307d58747c6b
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2021-09-24 14:27:19 -0300

    Beginner exercise: 3 digit numbers in an array

---
+++ Beginner Exercises/27_3DigitNumberArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _27_3DigitNumberArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 721, 9, 423, 56, 23, 34, 966, 78, 79, 3664, 12, 5516, 15 };
+
+            int threeDigit = 0;
+
+            foreach (int number in numbers)
+            {
+                if (number >= 100 && number <= 999)
+                    threeDigit++;
+            }
+
+            Console.WriteLine($"In the array, there are {threeDigit} numbers with 3 digits");
+
+            Console.ReadKey();
+        }
+    }
+}
