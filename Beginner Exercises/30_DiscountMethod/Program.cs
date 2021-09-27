bd5f0fdacb9cc214a58cfb8f404eebd0c286c372
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2021-09-26 22:13:05 -0300

    Beginner exercise: 20% discount with methods

---
+++ Beginner Exercises/30_DiscountMethod/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _30_DiscountMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintMessage("Enter the product's price: $", false);
+            float normalPrice = EnterPrice();
+
+            PrintMessage("", true);
+
+            PrintMessage($"The final price with 20% discount is: ${ApplyDiscount(normalPrice).ToString("00.00")}", true);
+
+            Console.ReadKey();
+        }
+
+        static float EnterPrice()
+        {
+            return float.Parse(Console.ReadLine());
+        }
+
+        static void PrintMessage(string message, bool skipLine)
+        {
+            if (skipLine)
+                Console.WriteLine(message);
+
+            else
+                Console.Write(message);
+        }
+
+        static float ApplyDiscount(float value)
+        {
+            return value = value - (value * 0.2f);
+        }
+    }
+}
