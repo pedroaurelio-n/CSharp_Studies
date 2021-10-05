d581dc3bb047624541d97e0e15d9c8325d64264a
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2021-10-05 18:18:35 -0300

    OOP study: Class methods

---
+++ OOP/OOP Studies/2_ClassMethods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _2_ClassMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User userOne = new User();
+            userOne.Username = "pedroaurelio_n";
+            userOne.FirstName = "Pedro";
+            userOne.LastName = "Aurelio";
+            userOne.Password = "123";
+
+            Console.WriteLine(User.WelcomeMessage(userOne.FirstName));
+            Console.WriteLine();
+
+            Console.Write("Enter your hourly salary: $");
+            userOne.SetHourlySalary(float.Parse(Console.ReadLine()));
+
+            Console.Write("Enter your work hours: ");
+            userOne.SetWorkHours(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine();
+
+            Console.WriteLine(userOne.FullName());
+            Console.WriteLine(userOne.LoginInformation());
+            Console.WriteLine(userOne.CalculateSalary());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OOP/OOP Studies/2_ClassMethods/User.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2_ClassMethods
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public float HourlySalary { get; private set; }
+        public int WorkHours { get; private set; }
+
+        private float _dailySalary;
+        private float _monthlySalary;
+        private float _yearlySalary;
+
+        public static string WelcomeMessage()
+        {
+            return "Welcome!";
+        }
+
+        public static string WelcomeMessage(string name)
+        {
+            return "Welcome, " + name + "!";
+        }
+
+        public string FullName()
+        {
+            return FirstName + " " + LastName;
+        }
+
+        public string LoginInformation()
+        {
+            return "Username: " + Username
+                    + "\nPassword: " + Password;
+        }
+
+        public void SetHourlySalary(float hourlySalary)
+        {
+            HourlySalary = hourlySalary;
+        }
+
+        public void SetWorkHours(int workHours)
+        {
+            WorkHours = workHours;
+        }
+
+        private float DailySalary()
+        {
+            _dailySalary = HourlySalary * WorkHours;
+            return _dailySalary;
+        }
+
+        private float MonthlySalary()
+        {
+            _monthlySalary = _dailySalary * 30;
+            return _monthlySalary;
+        }
+
+        private float YearlySalary()
+        {
+            _yearlySalary = _monthlySalary * 12;
+            return _yearlySalary;
+        }
+
+        public string CalculateSalary()
+        {
+            DailySalary();
+            MonthlySalary();
+            YearlySalary();
+
+            return "Daily salary:   $" + _dailySalary.ToString("0.00")
+               + "\nMonthly salary: $" + _monthlySalary.ToString("0.00")
+               + "\nYearly salary:  $" + _yearlySalary.ToString("0.00");
+        }
+    }
+}
