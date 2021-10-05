e612511edae9d0d218bf026d1ab6264f6c95462a
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2021-10-05 18:56:42 -0300

    OOP study: Encapsulation

---
+++ OOP/OOP Studies/3_Encapsulation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _3_Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User userOne = new User();
+            userOne.SetUsername("pedroaurelio_n");
+            userOne.SetFirstName("Pedro");
+            userOne.SetLastName("Aurelio");
+            userOne.SetPassword("123");
+
+            Console.WriteLine(User.WelcomeMessage(userOne.GetFirstName()));
+            Console.WriteLine();
+
+            Console.Write("Enter your hourly salary: $");
+            userOne.SetHourlySalary(float.Parse(Console.ReadLine()));
+
+            Console.Write("Enter your work hours: ");
+            userOne.SetWorkHours(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine();
+
+            Console.WriteLine(userOne.FullName());
+            Console.WriteLine(userOne.LoginInformation());
+            Console.WriteLine(userOne.CalculateSalary());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OOP/OOP Studies/3_Encapsulation/User.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3_Encapsulation
+{
+    public class User
+    {
+        private string _username;
+        private string _firstName;
+        private string _lastName;
+        private string _password;
+        private float _hourlySalary;
+        private int _workHours;
+
+        private float _dailySalary;
+        private float _monthlySalary;
+        private float _yearlySalary;
+
+        public string GetUsername() { return _username; }
+        public void SetUsername(string username) { _username = username; }
+
+        public string GetFirstName() { return _firstName; }
+        public void SetFirstName(string firstName) { _firstName = firstName; }
+
+        public string GetLastName() { return _lastName; }
+        public void SetLastName(string lastName) { _lastName = lastName; }
+
+        public string GetPassword() { return _password; }
+        public void SetPassword(string password) { _password = password; }
+
+        public float GetHourlySalary() { return _hourlySalary; }
+        public void SetHourlySalary(float hourlySalary) 
+        {
+            if (hourlySalary >= 0)
+                _hourlySalary = hourlySalary;
+
+            else
+                _hourlySalary = 0;
+        }
+
+        public int GetWorkHours() { return _workHours; }
+        public void SetWorkHours(int workHours)
+        {
+            if (workHours > 0)
+                _workHours = workHours;
+
+            else
+                _workHours = 0;
+        }
+
+
+        public static string WelcomeMessage()
+        {
+            return "Welcome!";
+        }
+
+        public static string WelcomeMessage(string name)
+        {
+            return "Welcome, " + name + "!";
+        }
+
+        public string FullName()
+        {
+            return _firstName + " " + _lastName;
+        }
+
+        public string LoginInformation()
+        {
+            return "Username: " + _username
+                    + "\nPassword: " + _password;
+        }
+
+        private float DailySalary()
+        {
+            _dailySalary = _hourlySalary * _workHours;
+            return _dailySalary;
+        }
+
+        private float MonthlySalary()
+        {
+            _monthlySalary = _dailySalary * 30;
+            return _monthlySalary;
+        }
+
+        private float YearlySalary()
+        {
+            _yearlySalary = _monthlySalary * 12;
+            return _yearlySalary;
+        }
+
+        public string CalculateSalary()
+        {
+            DailySalary();
+            MonthlySalary();
+            YearlySalary();
+
+            return "Daily salary:   $" + _dailySalary.ToString("0.00")
+               + "\nMonthly salary: $" + _monthlySalary.ToString("0.00")
+               + "\nYearly salary:  $" + _yearlySalary.ToString("0.00");
+        }
+    }
+}
