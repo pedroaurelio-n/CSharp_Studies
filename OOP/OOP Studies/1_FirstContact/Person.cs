7faf6e9e3e256d5b96aeaa2bbe01c76ce91ae954
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2021-10-05 17:30:19 -0300

    OOP Study: First contact

---
+++ OOP/OOP Studies/1_FirstContact/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1_FirstContact
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public float Balance { get; set; }
+
+        public void ShowInformation()
+        {
+            Console.WriteLine($"Name:       {FirstName} {LastName}\n" +
+                              $"Age:        {Age}\n" +
+                              $"Balance:    ${Balance.ToString("0.00")}");
+        }
+    }
+}
---
+++ OOP/OOP Studies/1_FirstContact/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _1_FirstContact
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person personOne = new Person();
+
+            personOne.FirstName = "Pedro";
+            personOne.LastName = "Aurelio";
+            personOne.Age = 20;
+            personOne.Balance = 0;
+
+            Person personTwo = new Person();
+
+            personTwo.FirstName = "Lalo";
+            personTwo.LastName = "Salamanca";
+            personTwo.Age = 34;
+            personTwo.Balance = 5000;
+
+            personOne.ShowInformation();
+            Console.WriteLine();
+            personTwo.ShowInformation();
+
+            Console.ReadKey();
+        }
+    }
+}
