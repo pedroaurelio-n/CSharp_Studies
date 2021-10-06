4b3f2d0f4294dd4704436a088cd76bb5f10bd9f0
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2021-10-05 21:51:11 -0300

    OOP exercise: Bank acount

---
+++ OOP/OOP Exercises/01_Account/Account.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_Account
+{
+    public class Account
+    {
+        private string _name;
+        private float _balance;
+
+        public Account() { }
+
+        public Account(string name, float balance)
+        {
+            _name = name;
+            _balance = balance;
+        }
+
+        public string GetName() { return _name; }
+        public void SetName(string name) { _name = name; }
+
+        public float GetBalance() { return _balance; }
+        public void SetBalance(float balance)
+        {
+            if (balance > 0)
+                _balance = balance;
+
+            else
+                _balance = 0;
+        }
+
+        public void DepositMoney(float value)
+        {
+            if (value > 0)
+                _balance += value;
+
+            Console.WriteLine($"Your balance now is ${_balance.ToString("0.00")}");
+        }
+
+        public void WithdrawMoney(float value)
+        {
+            if (_balance >= value)
+                _balance -= value;
+
+            else
+                _balance = 0;
+
+            Console.WriteLine($"Your balance now is ${_balance.ToString("0.00")}");
+        }
+
+        public override string ToString()
+        {
+            return $"Account Info"
+               + $"\nName:      {_name}"
+               + $"\nBalance:   ${_balance.ToString("0.00")}";
+        }
+    }
+}
---
+++ OOP/OOP Exercises/01_Account/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _01_Account
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Create your bank account!\n");
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your balance: $");
+            float balance = float.Parse(Console.ReadLine());
+
+            Account account1 = new Account(name, balance);
+
+            Console.WriteLine("\nAccount created!\n");
+            Console.WriteLine(account1);
+
+            Console.WriteLine();
+
+            Console.Write("Enter a value to deposit: $");
+            account1.DepositMoney(float.Parse(Console.ReadLine()));
+
+            Console.WriteLine();
+            Console.WriteLine(account1);
+
+            Console.WriteLine();
+
+            Console.Write("Enter a value to withdraw: $");
+            account1.WithdrawMoney(float.Parse(Console.ReadLine()));
+
+            Console.WriteLine();
+            Console.WriteLine(account1);
+
+            Console.ReadKey();
+        }
+    }
+}
