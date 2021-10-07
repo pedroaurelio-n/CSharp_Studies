d97f8fd0208c97f8cc0aa8ec193b99d8957efd81
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2021-10-07 17:50:49 -0300

    Intermediate exercise: Try catch

---
+++ Intermediate Exercises/02_TryCatch/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _02_TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {     
+            Console.Write("Enter a number: ");
+
+            try
+            {
+                int x = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter a divisor: ");
+
+                try
+                {
+                    int y = int.Parse(Console.ReadLine());
+
+                    try
+                    {
+                        CheckNumber(x);
+                        Console.WriteLine($"\n{x} / {y} = {x / y}");
+                    }
+
+                    catch (DivideByZeroException)
+                    {
+                        Console.WriteLine("You can't divide by zero!");
+                    }
+
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+
+                catch (FormatException)
+                {
+                    Console.WriteLine("You must type a number!");
+                }
+            }
+
+            catch (FormatException)
+            {
+                Console.WriteLine("You must type a number!");
+            }
+
+            Console.ReadKey();
+        }
+
+        static void CheckNumber(float x)
+        {
+            if (x <= 10)
+                throw new Exception("Number not bigger than 10!");
+        }
+    }
+}
