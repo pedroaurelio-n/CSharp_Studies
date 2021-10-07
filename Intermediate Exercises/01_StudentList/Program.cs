7f9dce392aaf710397baecf41bd5dc8dca8d6c26
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2021-10-07 17:00:08 -0300

    Intermediate exercise: Student list

---
+++ Intermediate Exercises/01_StudentList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01_StudentList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> studentList = new List<Student>();
+
+            studentList.Add(new Student("Pedro", 8, 10));
+            studentList.Add(new Student("Herobreno", 7, 4));
+            studentList.Add(new Student("Lalo", 7, 13));
+            studentList.Add(new Student("Steak", 2, 5));
+            studentList.Add(new Student("Tony", -2, 3));
+
+            foreach (Student student in studentList)
+            {
+                student.CalculateAverageScore();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Intermediate Exercises/01_StudentList/Student.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_StudentList
+{
+    public class Student
+    {
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        private float _score1;
+        public float Score1
+        {
+            get => _score1;
+            set
+            {
+                if (value <= 0)
+                    _score1 = 0;
+
+                else if (value > 0 && value < 10)
+                    _score1 = value;
+
+                else
+                    _score1 = 10;
+            }
+        }
+
+        private float _score2;
+        public float Score2
+        {
+            get => _score2;
+            set
+            {
+                if (value <= 0)
+                    _score2 = 0;
+
+                else if (value > 0 && value < 10)
+                    _score2 = value;
+
+                else
+                    _score2 = 10;
+            }
+        }
+
+        public Student() { }
+        public Student(string name, int score1, int score2)
+        {
+            Name = name;
+            Score1 = score1;
+            Score2 = score2;
+        }
+
+        public float CalculateAverageScore()
+        {
+            float average = (Score1 + Score2) / 2;
+            CheckStatus(average);
+
+            return average;
+        }
+
+        private void CheckStatus(float average)
+        {
+            if (average >= 5)
+                Console.WriteLine($"Congratulations! Student {Name} succeeded with an average of {average}.");
+
+            else
+                Console.WriteLine($"Student {Name} failed with an average of {average}. Try again next time.");
+        }
+    }
+}
