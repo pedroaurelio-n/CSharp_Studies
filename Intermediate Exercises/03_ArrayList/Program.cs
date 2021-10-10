0e90043631869612a435780b115cd11b54648fa2
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2021-10-09 21:34:10 -0300

    Intermediate exercise: ArrayList

---
+++ Intermediate Exercises/03_ArrayList/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+
+namespace _03_ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+
+            arrayList.Add("test1");
+            arrayList.Add(50);
+            arrayList.Add('a');
+            arrayList.Add(54.9f);
+            arrayList.Add(true);
+
+            foreach(var value in arrayList)
+            {
+                Console.WriteLine(value);
+            }
+
+            Console.WriteLine();
+
+            arrayList.Remove(50);
+            arrayList.Insert(1, 51);
+            arrayList.Add(false);
+            arrayList.Reverse();
+
+            foreach (var value in arrayList)
+            {
+                Console.WriteLine(value);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
