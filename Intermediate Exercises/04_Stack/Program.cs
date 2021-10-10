d663b5a97dfc0c2ec8cc4a4c2aa359604dd38cce
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2021-10-09 21:44:29 -0300

    Intermediate exercise: Stack

---
+++ Intermediate Exercises/04_Stack/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+
+namespace _04_Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+
+            stack.Push("Monday");
+            stack.Push("Tuesday");
+            stack.Push("Wednesday");
+            stack.Push("Thursday");
+            stack.Push("Friday");
+            stack.Push("Saturday");
+            stack.Push("Sunday");
+
+            foreach (var value in stack)
+            {
+                Console.WriteLine(value);
+            }
+
+            Console.WriteLine();
+
+            var pop = stack.Pop();
+            var peek = stack.Peek();
+
+            foreach (var value in stack)
+            {
+                Console.WriteLine(value);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Pop is {pop}");
+            Console.WriteLine($"Peek is {peek}");
+
+            Console.ReadKey();
+        }
+    }
+}
