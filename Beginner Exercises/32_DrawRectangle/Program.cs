2d873f3429787d600d58350e2c3db50f56b5b5d0
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2021-10-11 14:46:35 -0300

    Beginner exercise: Draw rectangle

---
+++ Beginner Exercises/32_DrawRectangle/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _32_DrawRectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the height: ");
+            int height = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the width: ");
+            int width = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter a character to fill the rectangle: ");
+            char fill = char.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Rectangle with {height} of height and {width} of width: ");
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Console.Write(fill);
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
