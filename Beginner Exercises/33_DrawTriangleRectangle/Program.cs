6398efe0bd957174f47367f914944d86a49780d3
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2021-10-11 15:00:33 -0300

    Beginner exercise: Draw triangle rectangle

---
+++ Beginner Exercises/33_DrawTriangleRectangle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _33_DrawTriangleRectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the side of the triangle: ");
+            int size = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter a character to fill the triangle: ");
+            char fill = char.Parse(Console.ReadLine());
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(fill);
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
